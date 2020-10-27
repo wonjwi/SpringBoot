88cb6a3fcf8e781e4db538ed4f2d73b4bad9ae38
wonjwi <wonji616@gmail.com>
2020-10-27 15:29:52 +0900

    2020-10-27 SpringBoot practice03 update

--- boot03/src/main/java/com/edu/persistence/BoardRepository.java
+++ boot03/src/main/java/com/edu/persistence/BoardRepository.java
@@ -1,16 +1,45 @@
 package com.edu.persistence;
 
+import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.edu.dto.BoardDTO;
 
 public interface BoardRepository extends CrudRepository<BoardDTO, Long> {
 	
-	//제목에 의한 검색
-	public List<BoardDTO> findBoardDTOByTitle(String title);
-
+	public List<BoardDTO> findBoardDTOByTitle(String title); //제목에 의한 검색
 	public List<BoardDTO> findBoardDTOByWriter(String writer);
+	
+	//작성자에 대한 like % keyword %
+	public Collection<BoardDTO> findByWriterContaining(String writer);
+	
+	public Collection<BoardDTO> findByTitleContainingAndBnoGreaterThan(String title, Long bno);
+	
+	//Order by
+	public Collection<BoardDTO> findByBnoGreaterThanOrderByBnoDesc(Long bno);
+	
+	//페이징 처리, 정렬
+	//Where bno > ? order by bno desc limit ?, ?
+	public List<BoardDTO> findByBnoGreaterThanOrderByBnoDesc(Long bno, Pageable paging);
+	
+	//페이징 처리, Sort
+	//public List<BoardDTO> findByBnoGreaterThan(Long bno, Pageable paging);
+	
+	public Page<BoardDTO> findByBnoGreaterThan(Long bno, Pageable paging);
+	
+	//@Query()
+	//From에는 Entity를 기술해야 한다.
+	@Query("SELECT b FROM BoardDTO b WHERE b.title LIKE %?1% AND b.bno > 0 "
+			+ "ORDER BY b.bno DESC")
+	public List<BoardDTO> findByTitle(String title);
+	
+	@Query("SELECT b.bno, b.title, b.writer, b.regdate FROM BoardDTO b " 
+			+ "WHERE b.title LIKE %?1% AND b.bno > 100 ORDER BY b.bno DESC")
+	public List<Object[]> findByTitle2(String title);
 	
 }
--- boot03/src/test/java/com/edu/Boot03ApplicationTests.java
+++ boot03/src/test/java/com/edu/Boot03ApplicationTests.java
@@ -1,8 +1,16 @@
 package com.edu;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.edu.dto.BoardDTO;
 import com.edu.persistence.BoardRepository;
@@ -22,13 +30,62 @@ class Boot03ApplicationTests {
 			repo.save(boardDTO);
 		}
 	}
-	
 	@Test
 	public void testByTitle() {
 		//repo.find 뒤에 엔터티 타입을 지정 
 		//By 뒤에는 컬럼명을 이용해서 만든다.
 		repo.findBoardDTOByTitle("147번째 글").forEach(boardDTO -> System.out.println(boardDTO));
 		repo.findBoardDTOByWriter("user04").forEach(boardDTO -> System.out.println(boardDTO));
+	}
+	@Test
+	public void testByWriterContaining() {
+		Collection<BoardDTO> results = repo.findByWriterContaining("07");
+		results.forEach(board -> System.out.println(board));
+	}
+	@Test
+	public void testByTitleAndBno() {
+		//Where title like "%5%" and bno > 50;
+		Collection<BoardDTO> results = repo.findByTitleContainingAndBnoGreaterThan("5", 50L);
+		results.forEach(board -> System.out.println(board));
+	}
+	@Test
+	public void testBnoOrderBy() {
+		Collection<BoardDTO> results = repo.findByBnoGreaterThanOrderByBnoDesc(173L);
+		results.forEach(board -> System.out.println(board));
+	}
+	//페이징 처리
+	@Test
+	public void testBnoOrderByPaging() {
+		//spring boot 2.0.0 이전에는 Pageable paging = new PageRequest(0, 10);
+		//spring boot 2.0.0 이후에는 아래와 같이 사용
+		Pageable paging = PageRequest.of(3, 10);
+		
+		Collection<BoardDTO> results = repo.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
+		results.forEach(board -> System.out.println(board));
+	}
+	@Test
+	public void testBnoPagingSort() {
+//		Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+//		Collection<BoardDTO> results = repo.findByBnoGreaterThan(0L, paging);
+//		results.forEach(board -> System.out.println(board));
+		
+		Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
+		Page<BoardDTO> results = repo.findByBnoGreaterThan(0L, paging);
+		System.out.println("PAGE SIZE 	: " + results.getSize());
+		System.out.println("TOTAL PAGES	: " + results.getTotalPages());
+		System.out.println("TOTAL COUNT	: " + results.getTotalElements());
+		System.out.println("NEXT 		: " + results.nextPageable());
+		
+		List<BoardDTO> list = results.getContent();
+		list.forEach(board -> System.out.println(board));
+	}
+	@Test
+	public void testByTitle2() {
+		repo.findByTitle("177").forEach(board -> System.out.println(board));
+	}
+	@Test
+	public void testByTitle3() {
+		repo.findByTitle2("160").forEach(arr -> System.out.println(Arrays.toString(arr)));
 	}
 
 } // End - class Boot03ApplicationTests
